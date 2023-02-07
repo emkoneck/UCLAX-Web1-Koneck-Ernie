c24f608dc56954ae42b5dcf9f2c0c12a8e5f3e62
Ernie Koneck <emkoneck@gmail.com>
2023-02-06 20:30:51 -0800

    First Commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+/* React Router Dom ---------------------------*/
+import { HashRouter } from 'react-router-dom';
+
+/* Components ---------------------------*/
+import App from './App';
+
+/* Global CSS ---------------------------*/
+import './Common/css/normalize.css';
+import './Common/css/global.scss';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <HashRouter>
+        <App />
+    </HashRouter>
+);
